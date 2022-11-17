67cdad9d53725da47c98776d36435f7a87455a15
t00216714 <t00216714@1XXC025A584423C>
2022-11-17 09:46:41 +0000

    tests done

---
+++ MotorTaxTests/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPFMotorTax.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using WPFMotorTax.Models;
+
+namespace WPFMotorTax.Repositories.Tests
+{
+    [TestClass()]
+    public class UserRepositoryTests
+    {
+        [TestMethod()]
+        public void AuthenticateUserTest()
+        {
+            UserRepository user = new UserRepository();
+            Assert.IsTrue(user.AuthenticateUser(new NetworkCredential("151-CK-532", "502124")));
+
+
+        }
+
+        [TestMethod()]
+        public void GetByRegNoTest()
+        {
+
+
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            
+        }
+    }
+}
